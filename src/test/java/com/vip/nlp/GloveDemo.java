e193b7c991c11b9737cf41799d6239a862424cdc
michaelliu03 <841412988@qq.com>
2018-07-05 15:17:57 +0800

    update 代码

--- src/test/java/com/vip/nlp/GloveDemo.java
+++ src/test/java/com/vip/nlp/GloveDemo.java
@@ -1,5 +1,16 @@
 package com.vip.nlp;
 
+import com.vip.nlp.glove.Cooccurrence;
+import com.vip.nlp.glove.GloVe;
+import com.vip.nlp.glove.Vocabulary;
+import com.vip.nlp.util.Options;
+import com.vip.nlp.util.StoreFileObject;
+import org.jblas.DoubleMatrix;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
 /**
  * @author michael.liu
  * @Title: GloveDemo
@@ -11,7 +22,37 @@ package com.vip.nlp;
 
 public class GloveDemo {
 
-    public static void main(String [] args){
+    private static java.util.logging.Logger log = Logger.getLogger(GloveDemo.class.getName());
 
+    public static void train(){
+        String file = "data/output2.txt";
+
+        Options options = new Options();
+        options.debug = true;
+        log.info("正在执行，请稍后......");
+
+        Vocabulary vocab = GloVe.build_vocabulary(file, options);
+        options.window_size = 3;
+        List<Cooccurrence> c =  GloVe.build_cooccurrence(vocab, file, options);
+
+        options.iterations = 100;
+        options.vector_size = 10;
+        options.debug = true;
+        DoubleMatrix W = GloVe.train(vocab, c, options);
+
+        try {
+            StoreFileObject.writeObject("model/matrix.model", W);
+            StoreFileObject.writeObject("model/vocab.cab", vocab);
+        } catch (IOException e) {
+            log.info("train save failure!" + e.getMessage());
+        }
+
+       log.info("执行结束");
+    }
+
+
+
+    public static void main(String [] args){
+        train();
     }
 }
