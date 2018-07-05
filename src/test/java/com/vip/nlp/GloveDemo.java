07baac65fbf6658adf286fb9154efc0c3c688ca8
michaelliu03 <841412988@qq.com>
2018-07-05 11:43:22 +0800

    create the new project

---
+++ src/test/java/com/vip/nlp/GloveDemo.java
@@ -0,0 +1,17 @@
+package com.vip.nlp;
+
+/**
+ * @author michael.liu
+ * @Title: GloveDemo
+ * @ProjectName nlpbox
+ * @Description: TODO
+ * @date 2018/7/511:42
+ */
+
+
+public class GloveDemo {
+
+    public static void main(String [] args){
+
+    }
+}
